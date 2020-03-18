087830d00864a457b864950226b9fe5f8f5bf483
Katherine Eisen <katherineeisen@Katherines-iMac.local>
2020-03-18 14:40:35 -0400

    SPME file updates

--- analysis_scripts/SPME_script.R
+++ analysis_scripts/SPME_script.R
@@ -1,48 +1,41 @@
-library(reshape2)
 
 
-spme<-read.csv("data/SPME_data_no_blanks.csv",header=TRUE)
+#for comparing the three vs six flower samples, I counted the number of compounds that each sample had in total, and in each of the four compound classes:
+three.six<-read.csv("data/3_6_spmes.csv", header=TRUE)
 
-spme_wide<-dcast(spme, Sample~Name, sum, value.var="Area")
-spme_ID<-read.csv("data/SPME_info.csv",header=TRUE)
+#total:
+test <- wilcox.test(three.six[1:12,1], three.six[13:24,1], paired=TRUE)
+Zstat<-qnorm(test$p.value/2)
 
-#adding species and community info:
-spme_wide<-cbind(spme_wide, spme_ID[,2:4])
+#glvs:
+test<-wilcox.test(three.six[1:12,7], three.six[13:24,7], paired=TRUE)
+Zstat<-qnorm(test$p.value/2)
 
-CB323<-spme_wide[which(spme_wide$Site=="CB323"),]
-CB323.U.dose<-CB323[which(CB323$Type=="3.Fl"|CB323$Type=="6.Fl"),]
-CB323.U.dose<-CB323.U.dose[order(CB323.U.dose$Type),]
-CB323.U.dose.3<-CB323[which(CB323$Type=="3.Fl"),]
-CB323.U.dose.6<-CB323[which(CB323$Type=="6.Fl"),]
+#aromatics:
+test<-wilcox.test(three.six[1:12,6], three.six[13:24,6], paired=TRUE)
+Zstat<-qnorm(test$p.value/2)
 
-ts <- sapply(2:48,function(x) (t.test((CB323.U.dose[1:3,x]), CB323.U.dose[4:6,x])))
-Pvalue <- t(as.matrix(ts[3,]))
+#sesquis:
+test <- wilcox.test(three.six[1:12,5], three.six[13:24,5], paired=TRUE)
+Zstat<-qnorm(test$p.value/2)
 
-CB323<-spme_wide[which(spme_wide$Site=="CB323"),]
-CB323.U.petals<-CB323[which(CB323$Type=="Petals"|CB323$Type=="Not Petals"),]
-CB323.U.petals<-CB323.U.petals[order(CB323.U.petals$Type),]
+#monos: 
+wilcoxon(three.six[1:12,4], three.six[13:24,4], paired=TRUE)
+Zstat<-qnorm(test$p.value/2)
 
-ts <- sapply(2:48,function(x) (t.test((CB323.U.petals[1:3,x]), CB323.U.petals[4:6,x])))
-Pvalue <- t(as.matrix(ts[3,]))
+#for petals:
 
+matrix<-read.csv("data/spme_petals_matrix.csv", header=TRUE)
 
-SC.U<-spme_wide[which(spme_wide$Site=="SC"&spme_wide$Species=="Ung"),]
-SC.U.petals<-SC.U[which(SC.U$Type=="Petals"|SC.U$Type=="Not Petals"),]
-SC.U.petals<-SC.U.petals[order(SC.U.petals$Type),]
+#removing totals and sample ID info
+scent.matrix<-matrix[,c(10:50)]
+petals<-matrix$Sample.Type
 
-ts <- sapply(2:48,function(x) (t.test((SC.U.petals[1:3,x]), SC.U.petals[4:6,x])))
-Pvalue <- t(as.matrix(ts[3,]))
+scent.dist<-vegdist(scent.matrix, method='jaccard')
 
-SC.C<-spme_wide[which(spme_wide$Site=="SC"&spme_wide$Species=="Cyl"),]
-SC.C.petals<-SC.C[which(SC.C$Type=="Petals"|SC.C$Type=="Not Petals"),]
-SC.C.petals<-SC.C.petals[order(SC.C.petals$Type),]
+scent.div<-adonis2(scent.dist~petals, data=matrix, permutations = 999, method="jaccard", strata="PLOT")
+scent.div
 
-ts <- sapply(2:48,function(x) (t.test((SC.C.petals[1:3,x]), SC.C.petals[4:6,x],paired=TRUE)))
-Pvalue <- t(as.matrix(ts[3,]))
-
-MHG.C<-spme_wide[which(spme_wide$Site=="MHG"&spme_wide$Species=="Cyl"),]
-MHG.C.petals<-MHG.C[which(MHG.C$Type=="Petals"|MHG.C$Type=="Not Petals"),]
-MHG.C.petals<-MHG.C.petals[order(MHG.C.petals$Type),]
-
-ts <- sapply(2:48,function(x) (t.test((MHG.C.petals[1:3,x]), MHG.C.petals[4:6,x],paired=TRUE)))
-Pvalue <- t(as.matrix(ts[3,]))
+matrix<-matrix[order(matrix$Sample.Type),]
+test<-wilcox.test(matrix$Total[1:12], matrix$Total[13:24], paired=TRUE)
+Zstat<-qnorm(test$p.value/2)
